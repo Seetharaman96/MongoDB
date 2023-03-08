db872b386a17c71edfa0ba26fc6844a937e9d7be
Seetharaman96 <seetharam.ad96@gmail.com>
2023-03-08 18:36:37 +0530

    upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import express, { request } from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 import moviesRouter from "./router/movies.router.js";
 import cors from 'cors';
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -26,5 +27,29 @@ app.get("/", function (request, response) {
 });
 
 app.use("/movies", moviesRouter);
-
+const mobiles = [
+  {
+    "model": "OnePlus 9 5G",
+    "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+    "company": "Oneplus"
+  },
+  {
+    "model": "Iphone 13 mini",
+    "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+    "company": "Apple"
+  },
+  {
+    "model": "Samsung s21 ultra",
+    "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+    "company": "Samsung"
+  },
+  {
+    "model": "Xiomi mi 11",
+    "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+    "company": "Xiomi"
+  }
+]
+app.get("/mobiles", function(request, response){
+    response.send(mobiles);
+})
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
