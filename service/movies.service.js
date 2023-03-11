c6c088b8d276f111281970b8e9467dd9568e40ce
Seetharaman96 <seetharam.ad96@gmail.com>
2023-03-11 13:20:55 +0530

    update

--- service/movies.service.js
+++ service/movies.service.js
@@ -1,16 +1,17 @@
+import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function deleteMovieById(id) {
-      return await client.db("b42wd2").collection("movies").deleteOne({ id: id });
+      return await client.db("b42wd2").collection("movies").deleteOne({ _id: new ObjectId(id) });
 }
 export async function updateMovieById(id, data2) {
-      return await client.db("b42wd2").collection("movies").updateOne({ id: id }, { $set: data2 });
+      return await client.db("b42wd2").collection("movies").updateOne({ _id: new ObjectId(id) }, { $set: data2 });
 }
 export async function createMovies(data) {
       return await client.db("b42wd2").collection("movies").insertOne(data);
 }
 export async function getMovieById(id) {
-      return await client.db("b42wd2").collection("movies").findOne({ id: id });
+      return await client.db("b42wd2").collection("movies").findOne({ _id: new ObjectId(id) });
 }
 export async function getAllMovies(query) {
       return await client.db("b42wd2").collection("movies").find(query).toArray();
